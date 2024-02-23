0c05850ed3c5b0d2699aec3aa5dea3133a240383
ignasgin <ignasgin@gmail.com>
2024-02-23 16:12:00 +0200

    Initial commit

---
+++ Board game/WebForm1.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Board_game.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Board games</title>
+    <style>
+        body {
+            overflow: hidden;
+            position: relative;
+        }
+
+        .button-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+        }
+
+        .button-container .button {
+            margin: 0 10px;
+        }
+
+        #StartingLabel {
+            position: absolute;
+            top: 25%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: calc(2vw + 2vh);
+        }
+
+        #GameLabel {
+            position: absolute;
+            top: 30px;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: calc(2vw + 2vh);
+
+        }
+
+        #GoBack {
+            position: fixed;
+            top: 10px;
+            left: 10px;
+        }
+
+        #Settings {
+            position: fixed;
+            top: 10px;
+            right: 10px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="StartingLabel" runat="server" Text="Choose your board game"></asp:Label>
+        <asp:Label ID="GameLabel" runat="server" Text=""></asp:Label> 
+        <asp:Button ID="GoBack" runat="server" Text="Go Back" Visible="false" OnClick="GoBack_Click"/>
+        <asp:Button ID="Settings" runat="server" Text="Settings" OnClick="Settings_Click"/>
+        <div class="button-container">
+            <asp:Button ID="Checkers" CssClass="button" runat="server" Text="Checkers" OnClick="Checkers_Click"/>
+            <asp:Button ID="Chess" CssClass="button" runat="server" Text="Chess" OnClick="Chess_Click"/>
+            <asp:Button ID="Undecided" CssClass="button" runat="server" Text="Undecided" OnClick="Undecided_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Board game/WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Board_game
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Shows the settings of the page (in progress)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Settings_Click(object sender, EventArgs e)
+        {
+            
+        }
+        /// <summary>
+        /// Returns user to starting page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void GoBack_Click(object sender, EventArgs e)
+        {
+            GoBack.Visible= false;
+            Checkers.Visible = true;
+            Chess.Visible = true;
+            Undecided.Visible = true;
+            StartingLabel.Visible = true;
+            GameLabel.Text = "";
+        }
+        /// <summary>
+        /// Shows the checkers board and initiates game once user makes a move (in progress)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Checkers_Click(object sender, EventArgs e)
+        {
+            GoBack.Visible = true;
+            Checkers.Visible = false;
+            Chess.Visible = false;
+            Undecided.Visible = false;
+            StartingLabel.Visible = false;
+            GameLabel.Text = "Checkers";
+        }
+        /// <summary>
+        /// Shows the chess board and initiates game once user makes a move (in progress)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Chess_Click(object sender, EventArgs e)
+        {
+            GoBack.Visible = true;
+            Checkers.Visible = false;
+            Chess.Visible = false;
+            Undecided.Visible = false;
+            StartingLabel.Visible = false;
+            GameLabel.Text = "Chess";
+        }
+        /// <summary>
+        /// Shows the undecided board and initiates game once user makes a move (in progress)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Undecided_Click(object sender, EventArgs e)
+        {
+            GoBack.Visible = true;
+            Checkers.Visible = false;
+            Chess.Visible = false;
+            Undecided.Visible = false;
+            StartingLabel.Visible = false;
+            GameLabel.Text = "Undecided";
+        }
+    }
+}
